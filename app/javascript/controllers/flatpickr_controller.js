b46d9ff4b13bd13dc35a6c7f3571e3eddc09236c
Alireza Habibi <habibi.alireza2010@gmail.com>
2023-06-06 22:56:19 +0200

    fix date disable

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -6,30 +6,29 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   static targets = [ "startTime", "endTime" ]
+  static values = {
+    startTime: String,
+    endTime: String,
+  }
   connect() {
+    const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
     flatpickr(this.startTimeTarget, {
+              minDate: 'today',
+              // maxDate: new Date().fp_incr(7), // 14 days from now
               dateFormat: "Y-m-d",
-              // defaultDate: ["2024-10-11", "2024-10-10"],
-              disable: [
-                function(date) {
-                    // disable every multiple of 8
-                    return !(date.getDate() % 8);
-                }
-              ],
+              // defaultDate: ['today',  new Date().fp_incr(7)],
+              disable: unvailableDates,
               // Provide an id for the plugin to work
-              plugins: [new rangePlugin({ input: "#endtime"})]
+              plugins: [new rangePlugin({ input: this.endTimeValue})]
             })
     flatpickr(this.endTimeTarget, {
+              minDate: 'today',
+              // maxDate: new Date().fp_incr(7), // 14 days from now
               dateFormat: "Y-m-d",
               // defaultDate: ["2024-10-11", "2024-10-10"],
-              disable: [
-                function(date) {
-                    // disable every multiple of 8
-                    return !(date.getDate() % 8);
-                }
-              ],
+              disable: unvailableDates,
               // Provide an id for the plugin to work
-              plugins: [new rangePlugin({ input: "#starttime"})]
+              plugins: [new rangePlugin({ input: this.startTimeValue})]
              })
   }
 }
